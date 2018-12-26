07b291f1a37eba8f33bcec17c4721bb80857cd7d
Moiz.Kachwala <moizk@hotmail.com>
2018-12-26 18:48:06 +0530

    fixes related to datepicker

--- src/components/common/DatePicker/DatePicker.jsx
+++ src/components/common/DatePicker/DatePicker.jsx
@@ -1,64 +1,29 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import DatePicker from 'react-datepicker';
 import moment from 'moment';
 import classnames from 'classnames';
-import {DATE_FORMAT} from '../../../store/constants/date';
+import { DATE_FORMAT } from '../../../store/constants/date';
 
 import './DatePicker.scss';
 
-class CustomDatePicker extends Component {
-    static propTypes = {
-        input: PropTypes.shape({
-            onChange: PropTypes.func.isRequired
-        }).isRequired,
-        meta: PropTypes.shape({
-            touched: PropTypes.bool,
-            error: PropTypes.string,
-        }),
-        placeholder: PropTypes.string,
-    }
-
-    static defaultProps = {
-        placeholder: '',
-    }
-
-    constructor(props) {
-        super(props);
-        this.handleChange = this.handleChange.bind(this);
-    }
-
-    handleChange(date) {
-        this.props.input.onChange(moment(date).format(DATE_FORMAT));
-    }
-
-    render() {
-        const {
-            input, placeholder, label, disabled,
-            meta: { touched, error }
-        } = this.props;
-
-        return (
-            <div className={classnames('form-group', {
-                'has-error': Boolean(touched && error),
-            })}>
-                <label>{label}</label>
-                <DatePicker
-                    className="form-control"
-                    {...input}
-                    placeholderText={placeholder}
-                    dateFormat={DATE_FORMAT}
-                    selected={input.value ? moment(input.value) : null}
-                    onChange={this.handleChange}
-                    autoComplete="off"
-                    showYearDropdown
-                    scrollableYearDropdown
-                    disabled={disabled}
-                />
-                {touched && error && <span className="error text-danger">{error}</span>}
-            </div>
-        );
-    }
-}
+const CustomDatePicker = ({ input, label, disabled, placeholder, defaultValue, meta: { touched, error } }) => (
+    <div className={classnames('form-group', {
+        'has-error': Boolean(touched && error),
+    })}>
+        <label>{label}</label>
+        <DatePicker
+            {...input}
+            dateFormat={DATE_FORMAT}
+            selected={input.value ? moment(input.value) : null}
+            autoComplete="off"
+            className="form-control"
+            placeholderText={placeholder}
+            disabled={disabled}
+            showYearDropdown
+        />
+        {touched && error && <span>{error}</span>}
+    </div>
+);
 
 export default CustomDatePicker;
